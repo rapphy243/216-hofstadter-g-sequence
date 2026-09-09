080699576b3360206fb306b0c2421a17a9ead12e
Rapphy <36800640+rapphy243@users.noreply.github.com>
2026-09-09 17:48:58 -0500

    Create functions

---
+++ src/sequence.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+int nativeSequence(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    return n - nativeSequence((nativeSequence(n - 1)));
+}
+
+int optimizedSequence(int n, std::vector<int>& arr {
+    if (n <= 0) {
+        return 0;
+    }
+
+    if (arr[n] != -1) {
+        return arr[n];
+    }
+
+    int value = optimizedSequence(optimizedSequence(n - 1, arr), arr);
+
+    arr[n] = n - value;
+    return arr[n];
+}
+
+int optimizedSequence(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+
+    std::vector<int> arr(n + 1, -1);
+    arr[0] = 0;
+
+    return optimizedSequence(n, arr);
+}
---
+++ src/sequence.hpp
@@ -0,0 +1,7 @@
+#include <unordered_map>
+
+int nativeSequence(int n);
+
+int optimizedSequence(int n, std::unordered_map<int, int>& map);
+    
+int optimizedSequence(int n);
